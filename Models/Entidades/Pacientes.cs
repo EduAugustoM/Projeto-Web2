ad68130c9144129d18e39a08dec4fb9a71569b83
EduAugustoM <e.duardoaugusto@hotmail.com>
2025-09-17 13:45:44 -0300

    First commit

---
+++ Models/Entidades/Pacientes.cs
@@ -0,0 +1,13 @@
+
+
+namespace Models.Enidades;
+
+public class Pacientes
+{    
+    public int codp { get; set; }
+    public string nome { get; set; } = string.Empty;
+    public int idade { get; set; }
+    public string cidade { get; set; } = string.Empty;
+    public string CPF { get; set; } = string.Empty;
+    public string doenca { get; set; } = string.Empty;
+}
